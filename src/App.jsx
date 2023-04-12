3408ac89ee3c06f99e454d0c5986abf9597db413
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-04-12 16:14:34 -0400

    landing page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Landing from './Landing'
+
 
 function App() {
   
 
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Landing />
    
     </div>
   )
---
+++ src/Landing.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+
+export default function Landing() {
+  return (
+    <div className = "landing-container">
+        <div className = "landing-text">
+            <h1>Quizzical</h1>
+            <h6>Give your brain a workout</h6>
+            <button className = "start-button">Start Quiz</button>
+        </div>
+       
+    </div>
+  )
+}
