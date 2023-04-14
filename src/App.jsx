350d9e8032690b1891e3d0d754bdf6cac344d8db
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-04-14 14:06:08 -0400

    dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,62 @@
-import { useState } from 'react'
-import Landing from './Landing'
+import React, { useState } from 'react'
+import { DarkModeToggle } from '@anatoliygatt/dark-mode-toggle';
+
+function Toggle(props) {
+  const [mode, setMode] = useState('dark');
+  return (
+    <DarkModeToggle
+      mode={mode}
+      dark="Dark"
+      light="Light"
+      size="md"
+      inactiveTrackColor="#e2e8f0"
+      inactiveTrackColorOnHover="#f8fafc"
+      inactiveTrackColorOnActive="#cbd5e1"
+      activeTrackColor="#334155"
+      activeTrackColorOnHover="#1e293b"
+      activeTrackColorOnActive="#0f172a"
+      inactiveThumbColor="#1e293b"
+      activeThumbColor="#e2e8f0"
+      onChange={(mode) => {
+        setMode(mode);
+      }} 
+      onClick = {props.onClick}
+      
+    />
+  );
+}
 
 
 function App() {
-  
+
+
+  const [isDarkMode, setIsDarkMode] = useState(false)
+
+  function toggleDarkMode() {
+    setIsDarkMode(!isDarkMode)
+  }
+
+ 
+
+
+
 
   return (
-    <div className="App">
-      <Landing />
-   
+    
+    <div className = {isDarkMode ? "dark-mode" : "light-mode"}>
+      <Toggle onClick = {toggleDarkMode} mode/>
+      <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
+        <div className = {`landing-container ${isDarkMode ? "dark-mode" : "light-mode"}`}>
+          <div className = {`landing-text ${isDarkMode ? "dark-mode" : "light-mode"}`}>
+              <h1>Quizzical</h1>
+              <h6>Give your brain a workout</h6>
+              <button className = "start-button" onClick={console.log("change screen")}>Start Quiz</button>
+          </div>
+        </div>
     </div>
+     
+    
+
   )
 }
 
--- src/Landing.jsx
+++ src/Landing.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 
 
-export default function Landing() {
+export default function Landing(props) {
   return (
     <div className = "landing-container">
         <div className = "landing-text">
